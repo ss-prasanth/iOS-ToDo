87ce6f55a69cc1546e023c15c7f2e3230d11b044
Prasanth Sivanappan <ssprasanth@gmail.com>
2013-08-13 18:10:41 +0530

    ToDo App.

---
+++ tyu/ToDo/ToDo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Prasanth Sivanappan on 08/08/13.
+//  Copyright (c) 2013 Prasanth Sivanappan. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tyu/ToDo/ToDoTests/ToDoTests.m
@@ -0,0 +1,32 @@
+//
+//  ToDoTests.m
+//  ToDoTests
+//
+//  Created by Prasanth Sivanappan on 08/08/13.
+//  Copyright (c) 2013 Prasanth Sivanappan. All rights reserved.
+//
+
+#import "ToDoTests.h"
+
+@implementation ToDoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ToDoTests");
+}
+
+@end
